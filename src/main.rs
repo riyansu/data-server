7e8a3cd691e60d16daa56556707536212b7bb672
Riyansu <kenKEN.DEM@gmail.com>
2025-08-20 15:45:53 +0900

    wip: ranking sytem

--- src/main.rs
+++ src/main.rs
@@ -20,11 +20,23 @@ struct ServerMsg{
     ranking: u16
 }
 
+fn calculate_ranking(team_data: &HashMap<String, u32>, target_id: &str) -> u16 {
+    let mut sorted: Vec<_> = team_data.iter().collect();
+    sorted.sort_by(|a, b| b.1.cmp(a.1)); // スコアの降順
+
+    for (i, (id, _)) in sorted.iter().enumerate() {
+        if *id == target_id {
+            return (i + 1) as u16;
+        }
+    }
+    0
+}
+
 
 #[tokio::main]
 async fn main() {
     let team_data:Arc<Mutex<HashMap<String,u32>>>        = Arc::new(Mutex::new(HashMap::new()));
-    dotenv().expect(".env file not found");
+    dotenv().expect("🚫 .env file not found");
     for (key,val) in env::vars(){
         if key == "IP"{
             println!("WebSocket >  ws://{}:9001",val);
@@ -34,28 +46,37 @@ async fn main() {
                 tokio::spawn(async move {
                     let ws_stream = accept_async(stream).await.unwrap();
                     let (mut write, mut read) = ws_stream.split();
-                    println!("connected");
+                    println!("-- Connected Successfully --");
                     write.send(Message::Text("connected".to_owned())).await.unwrap();
                     while let Some(msg) = read.next().await {
                         if let Ok(msg) = msg{     
                             if let Message::Text(text) = msg {
-                                // JSONをClientMessageに変換
                                 if let Ok(client_msg) = serde_json::from_str::<ClientMsg>(&text) {
+                                    let log = &client_msg;
                                     let mut data = team_data.lock().await;
+                                    let calc = data.clone();
                                     let score = data.entry(client_msg.id.clone()).or_insert(0);
+                                    println!("Record: Game{} - {} team - {}pt(+{}pt)",log.pc,log.id,log.score,&score);
                                     *score += client_msg.score;
                                     if client_msg.pc==3 {
+                                        let ranking = calculate_ranking(&calc, &client_msg.id);
+                                        if ranking==0 {
+                                            println!("⚠ Inconsistency occurred...")
+                                        }
                                         let server_msg = ServerMsg {
                                             pc: 4,
                                             id: client_msg.id.clone(),
                                             score:*score,
-                                            ranking:1,
+                                            ranking
                                         };
                                         let json = serde_json::to_string(&server_msg).unwrap();
+                                        println!("Result: {} team - {}pt", log.id, log.score);
                                         write.send(Message::Text(json)).await.unwrap();
+                                        data.remove(&client_msg.id);
+                                        println!("Remove: {} team", log.id);
                                     }
                                 } else {
-                                    println!("Invalid JSON: {}", text);
+                                    println!("⚠ Invalid JSON: {}", text);
                                 }
                             }
                         }
